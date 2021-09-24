6c0661ef699bd1757d6b580ae7a33b9b8072ce87
Matteo-Grellier <matteo-g@orange.fr>
2021-09-24 15:46:20 +0200

    Revert "Initialization of Player.java and Pieces.java"
    This reverts commit 011b2af9d0ec767b031547cf44651617957e020b.

--- puissance4/src/main/java/com/puissance4/Pieces.java
+++ puissance4/src/main/java/com/puissance4/Pieces.java
@@ -1,17 +1,5 @@
 package com.puissance4;
 
-public class Piece {
+public class Pieces {
     
-    private int colorOfPiece; //couleur de la pièce (celon un Enum)
-
-    private int lineIndex; //index de la ligne
-    private int columnIndex; //index de la colonne
-
-    public Piece(int colorOfPiece, int lineIndex, int columnIndex) {
-        this.colorOfPiece = colorOfPiece;
-        this.lineIndex = lineIndex;
-        this.columnIndex = columnIndex;
-    }
-
-
 }
--- puissance4/src/main/java/com/puissance4/Player.java
+++ puissance4/src/main/java/com/puissance4/Player.java
@@ -1,19 +1,5 @@
 package com.puissance4;
 
-import java.util.ArrayList;
-
-enum ColorOfPieces {
-    RED,
-    GREEN
-}
-
 public class Player {
-
-    private int teamColor;
-    private String name;
-
-    public Player(int teamColor, String name) {
-        this.teamColor = teamColor;
-        this.name = name;
-    }
+    
 }
