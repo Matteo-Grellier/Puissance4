011b2af9d0ec767b031547cf44651617957e020b
matheoleger <71370253+matheoleger@users.noreply.github.com>
2021-09-24 15:39:00 +0200

    Initialization of Player.java and Pieces.java

--- puissance4/src/main/java/com/puissance4/Pieces.java
+++ puissance4/src/main/java/com/puissance4/Pieces.java
@@ -1,5 +1,17 @@
 package com.puissance4;
 
-public class Pieces {
+public class Piece {
     
+    private int colorOfPiece; //couleur de la pièce (celon un Enum)
+
+    private int lineIndex; //index de la ligne
+    private int columnIndex; //index de la colonne
+
+    public Piece(int colorOfPiece, int lineIndex, int columnIndex) {
+        this.colorOfPiece = colorOfPiece;
+        this.lineIndex = lineIndex;
+        this.columnIndex = columnIndex;
+    }
+
+
 }
--- puissance4/src/main/java/com/puissance4/Player.java
+++ puissance4/src/main/java/com/puissance4/Player.java
@@ -1,5 +1,19 @@
 package com.puissance4;
 
+import java.util.ArrayList;
+
+enum ColorOfPieces {
+    RED,
+    GREEN
+}
+
 public class Player {
-    
+
+    private int teamColor;
+    private String name;
+
+    public Player(int teamColor, String name) {
+        this.teamColor = teamColor;
+        this.name = name;
+    }
 }
