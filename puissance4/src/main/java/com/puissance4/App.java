6566cffe007674b83ec058f9fc54b7cca811812f
matheoleger <71370253+matheoleger@users.noreply.github.com>
2021-09-27 16:11:30 +0200

    refacto arrayList
    change disposition of variable

--- puissance4/src/main/java/com/puissance4/App.java
+++ puissance4/src/main/java/com/puissance4/App.java
@@ -3,23 +3,22 @@ package com.puissance4;
 import java.util.*;
 public class App {
 
-    static int nbrColumns = 8;
+    static int nbrColumns = 7;
     static int nbrLines = 6;
 
+    private static ArrayList<ArrayList<Piece>> colonnes =  new ArrayList<ArrayList<Piece>>();
 
     public static void main(String[] args)
     {
-        
-        // Here aList is an ArrayList of ArrayLists
-        ArrayList<ArrayList<Integer>> colonnes =  new ArrayList<ArrayList<Integer>>();
-  
-        // Create n lists one by one and append to the 
-        // master list (ArrayList of ArrayList)
-        
+
         for (int i = 0; i < nbrColumns; i++) {
-            ArrayList<Integer> a1 = new ArrayList<Integer>();
-            colonnes.add(a1);
-            a1.add(i);
-        }  
+            ArrayList<Piece> newColumn = new ArrayList<Piece>();
+            colonnes.add(newColumn);
+        }
+
+    }
+
+    public static void round() {
+        
     }
 }
