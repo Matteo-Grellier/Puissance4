b69e367715d25be07e48e1fe34678d58ffd4e161
Matteo-Grellier <matteo-g@orange.fr>
2021-09-28 08:16:20 +0200

    Update App.java + Interface.java
    Création d'une première interface encore non utilisable pour insérer une pièce

--- puissance4/src/main/java/com/puissance4/App.java
+++ puissance4/src/main/java/com/puissance4/App.java
@@ -5,21 +5,22 @@ public class App {
 
     static int nbrColumns = 8;
     static int nbrLines = 6;
+    
 
 
     public static void main(String[] args)
     {
-        
-        // Here aList is an ArrayList of ArrayLists
-        ArrayList<ArrayList<Integer>> colonnes =  new ArrayList<ArrayList<Integer>>();
-  
-        // Create n lists one by one and append to the 
-        // master list (ArrayList of ArrayList)
-        
+        arr();
+    }
+
+    static ArrayList<Integer> arr(){
+        final ArrayList<ArrayList<Integer>> colonnes =  new ArrayList<ArrayList<Integer>>();
+        final ArrayList<Integer> lignes = new ArrayList<Integer>();
         for (int i = 0; i < nbrColumns; i++) {
-            ArrayList<Integer> a1 = new ArrayList<Integer>();
-            colonnes.add(a1);
-            a1.add(i);
-        }  
+            colonnes.add(lignes);
+            lignes.add(i);
+        }   
+        // System.out.println(colonnes);
+        return lignes; 
     }
 }
--- puissance4/src/main/java/com/puissance4/Interface.java
+++ puissance4/src/main/java/com/puissance4/Interface.java
@@ -1,5 +1,37 @@
 package com.puissance4;
 
+
 public class Interface {
     
+    public static void main(String[] args) {
+        Test();
+    }
+
+    static void Test(){
+
+        for (int i = 0; i < App.nbrLines; i++) {
+            for (int j = 0; j < App.nbrColumns; j++) {
+                System.out.print("|");
+                if (App.arr().get(j) == null) {
+                    System.out.print(" ");
+                }else {
+                    System.out.print(App.arr().get(j));
+                }
+            }
+            System.out.print("|");
+            System.out.println("");
+        }
+        for (int i = 0; i < App.nbrColumns*2; i++) {
+            System.out.print("#");
+        }
+        System.out.println("");
+        System.out.print("|");
+        for (int i = 1; i <= App.nbrColumns; i++) {
+            
+            System.out.print(i);
+            System.out.print("|");
+        }
+    }
+    
 }
+
